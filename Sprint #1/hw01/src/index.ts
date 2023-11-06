9f2f6a3c031e3e69a7ea361e7c6af7f80f35a4dc
ivansukhoparov <rbcdaaa@gmail.com>
2023-11-07 00:04:21 +0100

    добавил обработчики запросов согласно ТЗ

--- Sprint #1/hw01/src/index.ts
+++ Sprint #1/hw01/src/index.ts
@@ -1,1 +1,28 @@
-console.log('init test!');
+import express, {Request,Response} from "express";
+
+const app = express();
+const port:80 = 80;
+
+app.delete("/testing/all-data", (req:Request,res:Response):void=>{
+    res.send(204)
+})
+app.get("/videos", (req:Request,res:Response):void=>{
+    res.send("videos")
+})
+app.get("/videos/:id", (req:Request,res:Response):void=>{
+    res.send("videos")
+})
+app.post("/videos", (req:Request,res:Response):void=>{
+    res.send(200)
+})
+app.put("/videos/:id", (req:Request,res:Response):void=>{
+    res.send(200)
+})
+app.delete("/videos/:id", (req:Request,res:Response):void=>{
+    res.send(204)
+})
+
+app.listen(port, ():void=>{
+    console.log(`listening port: ${port}`);
+})
+
