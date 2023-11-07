f92ee1240bb1a03bcf6b9376845cac306870d4b2
ivansukhoparov <rbcdaaa@gmail.com>
2023-11-07 23:49:34 +0100

    update: move 'app' and handlers to settings.ts

--- Sprint #1/hw01/src/index.ts
+++ Sprint #1/hw01/src/index.ts
@@ -1,26 +1,7 @@
-import express, {Request,Response} from "express";
 
-const app = express();
 const port:80 = 80;
 
-app.delete("/testing/all-data", (req:Request,res:Response):void=>{
-    res.send(204)
-})
-app.get("/videos", (req:Request,res:Response):void=>{
-    res.send("videos")
-})
-app.get("/videos/:id", (req:Request,res:Response):void=>{
-    res.send("videos")
-})
-app.post("/videos", (req:Request,res:Response):void=>{
-    res.send(200)
-})
-app.put("/videos/:id", (req:Request,res:Response):void=>{
-    res.send(200)
-})
-app.delete("/videos/:id", (req:Request,res:Response):void=>{
-    res.send(204)
-})
+import {app} from "./settings";
 
 app.listen(port, ():void=>{
     console.log(`listening port: ${port}`);
--- Sprint #1/hw01/src/settings.ts
+++ Sprint #1/hw01/src/settings.ts
@@ -1,3 +1,5 @@
+import express, {Request,Response} from "express";
+export const app = express();
 
 const availableResolutions: string[] = [ "P144", "P240", "P360", "P480", "P720", "P1080", "P1440", "P2160" ]
 
@@ -15,3 +17,22 @@ const videos: VideoType[] = [
           ]
     }
 ]
+
+app.delete("/testing/all-data", (req:Request,res:Response):void=>{
+    res.send(204)
+})
+app.get("/videos", (req:Request,res:Response):void=>{
+    res.send("videos")
+})
+app.get("/videos/:id", (req:Request,res:Response):void=>{
+    res.send("videos")
+})
+app.post("/videos", (req:Request,res:Response):void=>{
+    res.send(200)
+})
+app.put("/videos/:id", (req:Request,res:Response):void=>{
+    res.send(200)
+})
+app.delete("/videos/:id", (req:Request,res:Response):void=>{
+    res.send(204)
+})
