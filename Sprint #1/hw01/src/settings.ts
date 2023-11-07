7d6160759c06d855a8dd459cf3409f317c246d23
ivansukhoparov <rbcdaaa@gmail.com>
2023-11-07 23:53:22 +0100

    create: create handler which send all videos

--- Sprint #1/hw01/src/settings.ts
+++ Sprint #1/hw01/src/settings.ts
@@ -1,7 +1,7 @@
 import express, {Request,Response} from "express";
 export const app = express();
 
-const availableResolutions: string[] = [ "P144", "P240", "P360", "P480", "P720", "P1080", "P1440", "P2160" ]
+const availableResolutions: string[] = [ "P144", "P240", "P360", "P480", "P720", "P1080", "P1440", "P2160" ];
 
 const videos: VideoType[] = [
     {
@@ -16,23 +16,23 @@ const videos: VideoType[] = [
               "P144"
           ]
     }
-]
+];
 
 app.delete("/testing/all-data", (req:Request,res:Response):void=>{
-    res.send(204)
+    res.send(204);
 })
 app.get("/videos", (req:Request,res:Response):void=>{
-    res.send("videos")
+    res.send(videos);
 })
 app.get("/videos/:id", (req:Request,res:Response):void=>{
-    res.send("videos")
+    res.send("videos by id");
 })
 app.post("/videos", (req:Request,res:Response):void=>{
-    res.send(200)
+    res.send(200);
 })
 app.put("/videos/:id", (req:Request,res:Response):void=>{
-    res.send(200)
+    res.send(200);
 })
 app.delete("/videos/:id", (req:Request,res:Response):void=>{
-    res.send(204)
+    res.send(204);
 })
