060a8ee5fb066e3a7076c77da4f1b84fb887e8e1
ivansukhoparov <rbcdaaa@gmail.com>
2023-11-08 21:56:47 +0100

    change: move type VideoType to settings.ts and delete types.ts

--- Sprint #1/hw01/src/settings.ts
+++ Sprint #1/hw01/src/settings.ts
@@ -3,6 +3,33 @@ export const app = express();
 
 const availableResolutions: string[] = [ "P144", "P240", "P360", "P480", "P720", "P1080", "P1440", "P2160" ];
 
+type VideoType ={
+    id: number
+    title:	string
+    author:	string
+    canBeDownloaded:	boolean
+    //By default - false
+
+    minAgeRestriction:	number |null
+    /*maximum: 18
+    minimum: 1
+    default: null
+    nullable: true
+    null - no restriction*/
+
+    createdAt: 	string //($date-time)
+    publicationDate:	string //($date-time)
+    //By default - +1 day from CreatedAt
+
+    availableResolutions: typeof availableResolutions
+    /*  nullable: true
+        h01.Resolutionsstring
+        Enum:
+            [ P144, P240, P360, P480, P720, P1080, P1440, P2160 ]
+        ]
+        */
+}
+
 const videos: VideoType[] = [
     {
           id : 0,
