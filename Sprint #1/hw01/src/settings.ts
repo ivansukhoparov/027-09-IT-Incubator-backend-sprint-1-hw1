8e9e39be553929dfa1f9c02f23ef218272e15b53
ivansukhoparov <rbcdaaa@gmail.com>
2023-11-09 01:44:50 +0100

    create: create handler for post requests

--- Sprint #1/hw01/src/settings.ts
+++ Sprint #1/hw01/src/settings.ts
@@ -1,7 +1,8 @@
 import express, {Request,Response} from "express";
 export const app = express();
 
-const availableResolutions: string[] = [ "P144", "P240", "P360", "P480", "P720", "P1080", "P1440", "P2160" ];
+const AvailableResolutions: string[] = [ "P144", "P240", "P360", "P480", "P720", "P1080", "P1440", "P2160" ];
+app.use(express.json());
 
 type VideoType ={
     id: number
@@ -21,7 +22,7 @@ type VideoType ={
     publicationDate:	string //($date-time)
     //By default - +1 day from CreatedAt
 
-    availableResolutions: typeof availableResolutions
+    availableResolutions: typeof AvailableResolutions
     /*  nullable: true
         h01.Resolutionsstring
         Enum:
@@ -30,7 +31,22 @@ type VideoType ={
         */
 }
 type RequestWithParams<P> = Request<P,{},{},{}>
-
+type RequestWithBody<B> = Request<{},{},B,{}>
+type Params ={
+    id: string
+}
+type Body = {
+    title: string
+    author: string
+    availableResolutions: typeof AvailableResolutions
+}
+type ErrorType = {
+    errorsMessages:ErrorsMessageType[]
+}
+type ErrorsMessageType = {
+    field:string
+    message:string
+}
 
 const videos: VideoType[] = [
     {
@@ -51,7 +67,7 @@ app.get("/videos", (req:Request,res:Response):void=>{
     res.send(videos);
 })
 
-app.get("/videos/:id", (req:RequestWithParams<{ id: string }> ,res:Response):void=>{
+app.get("/videos/:id", (req:RequestWithParams<Params> ,res:Response):void=>{
     const id: number = +req.params.id;
     const video:VideoType|undefined = videos.find((el)=> el.id === id);
     if (!video) {
@@ -61,15 +77,61 @@ app.get("/videos/:id", (req:RequestWithParams<{ id: string }> ,res:Response):voi
     }
 })
 
+app.post("/videos", (req:RequestWithBody<Body>,res:Response):void=>{
+    let errors:ErrorType = {
+        errorsMessages:[]
+    }
+
+    let {title,author,availableResolutions} = req.body;
+
+    if (!title || title.trim().length<1 || title.trim().length>40){
+        errors.errorsMessages.push({message:"Invalid title", field:"title"});
+    }
+
+    if (!author || author.trim().length<1 || author.trim().length>20){
+        errors.errorsMessages.push({message:"Invalid author", field:"author"});
+    }
+
+    if (Array.isArray(availableResolutions)){
+        availableResolutions.map((r)=>{
+            !AvailableResolutions.includes(r) && errors.errorsMessages.push({message:"Invalid availableResolutions", field:"availableResolutions"});
+        })
+    }else{
+        availableResolutions=[]
+    }
+
+    if (errors.errorsMessages.length){
+        res.status(400).send(errors)
+        return
+    }
+
+    const createdAt= new Date();
+    const publicationDate = new Date();
+
+    publicationDate.setDate(createdAt.getDate()+1);
+
+    const newVideo: VideoType = {
+        id : +(new Date()),
+        title :   title  ,
+        author :  author  ,
+        canBeDownloaded : false,
+        minAgeRestriction : null,
+        createdAt :   createdAt.toISOString()  ,
+        publicationDate :   publicationDate.toISOString()  ,
+        availableResolutions : availableResolutions
+    }
+
+    videos.push(newVideo);
+    res.status(201).send(newVideo);
+})
+
 
 /*
 app.delete("/testing/all-data", (req:Request,res:Response):void=>{
     res.send(204);
 })
 
-app.post("/videos", (req:Request,res:Response):void=>{
-    res.send(200);
-})
+
 app.put("/videos/:id", (req:Request,res:Response):void=>{
     res.send(200);
 })
