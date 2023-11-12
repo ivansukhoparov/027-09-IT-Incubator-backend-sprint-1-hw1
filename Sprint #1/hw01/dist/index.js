62851c1023be1474bba7dc77dc858f57d25cc9a2
ivansukhoparov <rbcdaaa@gmail.com>
2023-11-12 23:36:20 +0100

    move files

--- Sprint #1/hw01/dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const port = 80;
-const settings_1 = require("./settings");
-settings_1.app.listen(port, () => {
-    console.log(`listening port: ${port}`);
-});
